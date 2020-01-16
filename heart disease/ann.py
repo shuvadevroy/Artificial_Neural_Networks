04a193cc172beb08db151d6dccc97eab12e38a7a
Shuva Dev <58085886+shuvadevroy@users.noreply.github.com>
2020-01-16 17:49:54 +0600

    Add files via upload

---
+++ heart disease/ann.py
@@ -0,0 +1,58 @@
+
+
+# Importing the libraries
+
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(r"G:\NN Project\Artificial_Neural_Networks\heart.csv")
+dataset = dataset.sample(frac=1)
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+#import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu', input_dim = 13))
+
+# Adding the second hidden layer
+classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 1000)
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
